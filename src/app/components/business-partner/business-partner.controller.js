e4f0e000794b912487df050949f95f4f45309197
Damián García <damian.garcia@zalando.de>
2017-01-16 13:44:33 +0100

    chore(visibility): add example as reminder on how to connect to visibility actions

--- src/app/components/business-partner/business-partner.controller.js
+++ src/app/components/business-partner/business-partner.controller.js
@@ -1,3 +1,5 @@
+import * as Actions from '../../actions';
+
 class BusinessPartnerController {
 
   static mapStateToThis(state) {
@@ -18,6 +20,15 @@ class BusinessPartnerController {
       this.businessPartnerActions
     )(this);
   }
+
+  /* TODO This is just a fiddle to show that common visibility actions can be accessed independently from
+          different components but it must be implemented properly in a different issue, moving all the related
+          files to the common directory and maybe writing an own dedicated service to access them
+  toggleBusinessPartnerMenu() {
+    let dispatchAction = Actions['toggleBusinessPartnerMenu'];
+    this.$ngRedux.dispatch(dispatchAction());
+  }
+  */
 }
 
 export default BusinessPartnerController;
