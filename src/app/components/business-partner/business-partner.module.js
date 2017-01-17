0f3e2b87588ce6aaaf4337a2305e60e2a7737879
Greg Deane <greg.james.deane@zalando.de>
2017-01-17 16:42:35 +0100

    refactor(business-partner): remove unused service

--- src/app/components/business-partner/business-partner.module.js
+++ src/app/components/business-partner/business-partner.module.js
@@ -1,6 +1,5 @@
 import BusinessPartnerActions from './business-partner.actions';
 import BusinessPartnerComponent from './business-partner.component';
-import BusinessPartnerService from './business-partner.service';
 import BusinessPartnerMenuModule from './business-partner-menu/business-partner-menu.module';
 
 const BusinessPartnerModule =
@@ -8,7 +7,6 @@ const BusinessPartnerModule =
     BusinessPartnerMenuModule
   ])
   .factory('BusinessPartnerActions', BusinessPartnerActions)
-  .service('BusinessPartnerService', BusinessPartnerService)
   .component('businessPartner', BusinessPartnerComponent);
 
 export default BusinessPartnerModule.name;
