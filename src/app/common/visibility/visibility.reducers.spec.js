9b64114244d2e8b715310af6f5f7655191e95268
Greg Deane <greg.james.deane@zalando.de>
2017-01-17 16:22:45 +0100

    test(visibility): add visibility tests

---
+++ src/app/common/visibility/visibility.reducers.spec.js
@@ -0,0 +1,172 @@
+import * as constants from './visibility.constants';
+import visibility from './visibility.reducers';
+
+describe('Visibility Reducers', () => {
+  let mock;
+
+  beforeEach(setup);
+
+  it('should return current state for unknown action', () => {
+    const result = visibility(mock.initialState, {
+      type: 'random'
+    });
+
+    expect(result).toEqual(mock.initialState);
+  });
+
+  it(`should update appropriate values for ${constants.TOGGLE_APPLICATIONS_MENU} action type`, () => {
+    const result = visibility(mock.updatedState, {
+      type: constants.TOGGLE_APPLICATIONS_MENU
+    });
+
+    expect(result.applicationsMenuShown).toBe(!mock.updatedState.applicationsMenuShown);
+    expect(result.userMenuShown).toBe(false);
+  });
+
+  it(`should update appropriate values for ${constants.SHOW_APPLICATIONS_MENU} action type`, () => {
+    const result = visibility(mock.initialState, {
+      type: constants.SHOW_APPLICATIONS_MENU
+    });
+
+    expect(result.applicationsMenuShown).toBe(true);
+    expect(result.userMenuShown).toBe(false);
+  });
+
+  it(`should update appropriate values for ${constants.TOGGLE_USER_MENU} action type`, () => {
+    const result = visibility(mock.updatedState, {
+      type: constants.TOGGLE_USER_MENU
+    });
+
+    expect(result.userMenuShown).toBe(!mock.updatedState.userMenuShown);
+    expect(result.applicationsMenuShown).toBe(false);
+  });
+
+  it(`should update appropriate values for ${constants.SHOW_BUSINESS_PARTNER_MENU} action type`, () => {
+    const result = visibility(mock.initialState, {
+      type: constants.SHOW_BUSINESS_PARTNER_MENU
+    });
+
+    expect(result.businessPartnerMenuShown).toBe(true);
+    expect(result.mobileMenuShown).toBe(false);
+  });
+
+  it(`should update appropriate values for ${constants.HIDE_BUSINESS_PARTNER_MENU} action type`, () => {
+    const result = visibility(mock.updatedState, {
+      type: constants.HIDE_BUSINESS_PARTNER_MENU
+    });
+
+    expect(result.businessPartnerMenuShown).toBe(false);
+  });
+
+  it(`should update appropriate values for ${constants.TOGGLE_BUSINESS_PARTNER_MENU} action type`, () => {
+    const result = visibility(mock.updatedState, {
+      type: constants.TOGGLE_BUSINESS_PARTNER_MENU
+    });
+
+    expect(result.businessPartnerMenuShown).toBe(!mock.updatedState.businessPartnerMenuShown);
+    expect(result.mobileMenuShown).toBe(false);
+  });
+
+  it(`should update appropriate values for ${constants.DISABLE_BUSINESS_PARTNER_MENU} action type`, () => {
+    const result = visibility(mock.initialState, {
+      type: constants.DISABLE_BUSINESS_PARTNER_MENU
+    });
+
+    expect(result.businessPartnerMenuDisabled).toBe(true);
+  });
+
+  it(`should update appropriate values for ${constants.CHANGE_SELECTED_PRODUCT} action type`, () => {
+    const result = visibility(mock.initialState, {
+      type: constants.CHANGE_SELECTED_PRODUCT,
+      product: mock.data
+    });
+
+    expect(result.selectedProduct).toBe(mock.data);
+  });
+
+  it(`should update appropriate values for ${constants.RESET_SELECTED_PRODUCT} action type`, () => {
+    const result = visibility(mock.updatedState, {
+      type: constants.RESET_SELECTED_PRODUCT
+    });
+
+    expect(result.selectedProduct).toBeUndefined();
+  });
+
+  it(`should update appropriate values for ${constants.SET_HELP_WIDGET_AS_LOADED} action type`, () => {
+    const result = visibility(mock.initialState, {
+      type: constants.SET_HELP_WIDGET_AS_LOADED
+    });
+
+    expect(result.helpWidgetLoaded).toBe(true);
+  });
+
+  it(`should update appropriate values for ${constants.HIDE_HELP_WIDGET} action type`, () => {
+    const result = visibility(mock.updatedState, {
+      type: constants.HIDE_HELP_WIDGET
+    });
+
+    expect(result.helpWidgetShown).toBe(false);
+  });
+
+  it(`should update appropriate values for ${constants.TOGGLE_HELP_WIDGET} action type`, () => {
+    const result = visibility(mock.initialState, {
+      type: constants.TOGGLE_HELP_WIDGET
+    });
+
+    expect(result.helpWidgetShown).toBe(!mock.initialState.helpWidgetShown);
+  });
+
+  it(`should update appropriate values for ${constants.HIDE_ALL_MENUS} action type`, () => {
+    const result = visibility(mock.updatedState, {
+      type: constants.HIDE_ALL_MENUS
+    });
+
+    expect(result.applicationsMenuShown).toBe(false);
+    expect(result.userMenuShown).toBe(false);
+    expect(result.businessPartnerMenuShown).toBe(false);
+    expect(result.helpWidgetShown).toBe(false);
+    expect(result.mobileMenuShown).toBe(false);
+  });
+
+  it(`should update appropriate values for ${constants.TOGGLE_MOBILE_MENU} action type`, () => {
+    const result = visibility(mock.updatedState, {
+      type: constants.TOGGLE_MOBILE_MENU
+    });
+
+    expect(result.mobileMenuShown).toBe(!mock.updatedState.mobileMenuShown);
+    expect(result.businessPartnerMenuShown).toBe(false);
+  });
+
+  ////////////////////////////
+
+  function setup() {
+    mocks();
+  }
+
+  function mocks() {
+    mock = {
+      initialState: {
+        applicationsMenuShown: false,
+        userMenuShown: false,
+        selectedProduct: undefined,
+        businessPartnerMenuShown: false,
+        helpWidgetLoaded: false,
+        helpWidgetShown: false,
+        mobileMenuShown: false
+      },
+      updatedState: {
+        applicationsMenuShown: true,
+        userMenuShown: true,
+        selectedProduct: {},
+        businessPartnerMenuShown: true,
+        helpWidgetLoaded: true,
+        helpWidgetShown: true,
+        mobileMenuShown: true
+      },
+      data: {
+        id: 2,
+        name: 'Test'
+      }
+    };
+  }
+});
