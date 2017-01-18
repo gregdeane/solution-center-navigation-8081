847c8c917edc049ff888b4d39e22a02a29e343af
Damián García <damian.garcia@zalando.de>
2017-01-18 15:15:49 +0100

    feat(state-handler): centralise state handler methods

--- src/app/common/common.js
+++ src/app/common/common.js
@@ -3,15 +3,17 @@ import angular from 'angular';
 import BackendConnectorService from './backend-connector/backend-connector.service';
 import ModuleConnectorService from './backend-connector/module-connector.service';
 import InterceptorService from './security/interceptor.service';
+import StateHandlerService from './state-handler/state-handler.service';
 
 const commonModule =
-          angular.module('common', [
-            'solutioncenter.login',
-            'ngCookies'
-          ])
+        angular.module('common', [
+          'solutioncenter.login',
+          'ngCookies'
+        ])
           .service('interceptorService', InterceptorService)
           .service('backendConnectorService', BackendConnectorService)
           .service('moduleConnectorService', ModuleConnectorService)
+          .service('stateHandlerService', StateHandlerService)
           .config(($httpProvider) => {
             $httpProvider.interceptors.push('interceptorService');
           });
